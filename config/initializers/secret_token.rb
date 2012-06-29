a3f614d67821ddcbe971fa711572483c15a82554
phinze <paul.t.hinze@gmail.com>
2012-06-29 15:55:53 -0500

    rails new baby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tautologist::Application.config.secret_token = '58b3eec4ca0d88d3966e4536a20e1b1ca67c1b7d5be34171f13899c7755a5694d44257183de94a09c1d81d03ba9e0ee3ee15e5fd7125508be32d11a1074891e9'
