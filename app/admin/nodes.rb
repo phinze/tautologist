f4cf19e4965cd649a8b09634cc45d65d4a2311b6
phinze <paul.t.hinze@gmail.com>
2012-07-05 19:13:18 -0500

    make node show page list interfaces

--- app/admin/nodes.rb
+++ app/admin/nodes.rb
@@ -4,4 +4,21 @@ ActiveAdmin.register Node do
     column :location
     default_actions
   end
+
+  show do |node|
+    attributes_table do
+      row :title
+      row :location
+      row :interfaces do
+        ul do
+          node.network_interfaces.each do |interface|
+            li link_to(interface.name, admin_network_interface_path(interface)) + " (#{interface.address})"
+          end
+        end
+      end
+    end
+    active_admin_comments
+  end
+
+
 end
